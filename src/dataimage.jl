ca3a2513d7e84c341cb95a5cd82a0062ab525964
Shreesh Adiga <16567adigashreesh@gmail.com>
2020-10-08 08:23:21 +0530

    Fix typo: mahalabonis to mahalanobis

--- src/dataimage.jl
+++ src/dataimage.jl
@@ -2,16 +2,16 @@
 
     euclideanDistances(dataMatrix)
 
-Calculate Euclidean distances between pairs. 
+Calculate Euclidean distances between pairs.
 
 # Arguments
 - `dataMatrix::Array{Float64, 1}`: Data matrix with dimensions n x p, where n is the number of observations and p is the number of variables.
 
 # Notes
     This is the helper function for the dataimage() function defined in Marchette & Solka (2003).
-    
+
 # References
-Marchette, David J., and Jeffrey L. Solka. "Using data images for outlier detection." 
+Marchette, David J., and Jeffrey L. Solka. "Using data images for outlier detection."
 Computational Statistics & Data Analysis 43.4 (2003): 541-552.
 """
 function euclideanDistances(dataMatrix::Array{Float64,2})::Array{Float64,2}
@@ -19,7 +19,7 @@ function euclideanDistances(dataMatrix::Array{Float64,2})::Array{Float64,2}
     d = zeros(Float64, n, n)
     for i in 1:n
         for j in i:n
-            if i != j 
+            if i != j
                 d[i, j] = sqrt(sum((dataMatrix[i,:] .- dataMatrix[j,:]).^2.0))
                 d[j, i] = d[i, j]
             end
@@ -33,18 +33,18 @@ end
 
     mahalanobisBetweenPairs(dataMatrix)
 
-Calculate Mahalanobis distances between pairs. 
+Calculate Mahalanobis distances between pairs.
 
 # Arguments
 - `dataMatrix::Array{Float64, 1}`: Data matrix with dimensions n x p, where n is the number of observations and p is the number of variables.
 
 # Notes
-    Differently from Mahalabonis distances, this function calculates Mahalanobis distances between 
-    pairs, rather than the distances to center of the data. This is the helper function for the 
+    Differently from Mahalanobis distances, this function calculates Mahalanobis distances between
+    pairs, rather than the distances to center of the data. This is the helper function for the
     dataimage() function defined in Marchette & Solka (2003).
-    
+
 # References
-Marchette, David J., and Jeffrey L. Solka. "Using data images for outlier detection." 
+Marchette, David J., and Jeffrey L. Solka. "Using data images for outlier detection."
 Computational Statistics & Data Analysis 43.4 (2003): 541-552.
 """
 function mahalanobisBetweenPairs(dataMatrix::Array{Float64,2})::Array{Float64,2}
@@ -57,7 +57,7 @@ function mahalanobisBetweenPairs(dataMatrix::Array{Float64,2})::Array{Float64,2}
     covinv = inv(covmat)
     for i in 1:n
         for j in i:n
-            if i != j 
+            if i != j
                 d[i, j] = sqrt((dataMatrix[i,:] .- dataMatrix[j,:]) * covinv * (dataMatrix[i,:] .- dataMatrix[j,:])')
                 d[j, i] = d[i, j]
             end
@@ -72,20 +72,20 @@ end
 
     dataimage(dataMatrix; distance = :mahalanobis)
 
-Generate the Marchette & Solka (2003) data image for a given data matrix. 
+Generate the Marchette & Solka (2003) data image for a given data matrix.
 
 # Arguments
 - `dataMatrix::Array{Float64, 1}`: Data matrix with dimensions n x p, where n is the number of observations and p is the number of variables.
 - `distance::Symbol`: Optional argument for the distance function.
 
 # Notes
-    distance is :mahalanobis by default, for the Mahalanobis distances. 
-    use 
+    distance is :mahalanobis by default, for the Mahalanobis distances.
+    use
 
         dataimage(mat, distance = :euclidean)
-    
+
     to use Euclidean distances.
-    
+
 # Examples
 ```julia-repl
 julia> x1 = hbk[:,"x1"];
@@ -96,7 +96,7 @@ julia> dataimage(mat)
 ```
 
 # References
-Marchette, David J., and Jeffrey L. Solka. "Using data images for outlier detection." 
+Marchette, David J., and Jeffrey L. Solka. "Using data images for outlier detection."
 Computational Statistics & Data Analysis 43.4 (2003): 541-552.
 """
 function dataimage(dataMatrix::Array{Float64,2}; distance=:mahalanobis)
@@ -106,7 +106,7 @@ function dataimage(dataMatrix::Array{Float64,2}; distance=:mahalanobis)
     elseif distance == :euclidean
         d = euclideanDistances(dataMatrix)
     else
-        @error "Distance function unknown: " distance 
+        @error "Distance function unknown: " distance
         @error "Using mahalanobis instead"
         d = mahalanobisSquaredBetweenPairs(dataMatrix)
     end
